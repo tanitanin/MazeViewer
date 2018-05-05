6407306633215f4c6ac06a566553a4728db78a32
tanitanin <tajima2210@gmail.com>
2018-05-05 23:35:41 +0900

    修正

--- MazeViewer/Core/Wall.cs
+++ MazeViewer/Core/Wall.cs
@@ -14,28 +14,4 @@ namespace MazeViewer.Core
         public bool Exist { get; set; } = false;
         public bool Virtual { get; set; } = false;
     }
-
-    public class WallMaze
-    {
-        public int Size { get; set; } = 0;
-        public IEnumerable<Wall> VerticalWalls { get; set; }
-        public IEnumerable<Wall> HorizontalWalls { get; set; }
-
-        public Cell Start { get; set; }
-        public IEnumerable<Cell> Goals { get; set; }
-
-        public bool IsWall(int x, int y, DirectionType direction)
-        {
-            switch (direction)
-            {
-                case DirectionType.North:
-
-                case DirectionType.South:
-                case DirectionType.East:
-                case DirectionType.West:
-                default:
-                    return false;
-            }
-        }
-    }
 }
---
+++ MazeViewer/Core/WallMaze.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeViewer.Core
+{
+
+    public class WallMaze
+    {
+        public int Size { get; set; } = 0;
+        public List<Wall> VerticalWalls { get; set; }
+        public List<Wall> HorizontalWalls { get; set; }
+
+        public Cell Start { get; set; }
+        public IEnumerable<Cell> Goals { get; set; }
+        
+        public Wall At(int x, int y, DirectionType direction)
+        {
+            switch (direction)
+            {
+                case DirectionType.North: return HorizontalWalls[x + y * (Size + 1)];
+                case DirectionType.South: return HorizontalWalls[x + (y + 1) * (Size + 1)];
+                case DirectionType.East: return VerticalWalls[(x + 1) + y * (Size + 1)];
+                case DirectionType.West: return VerticalWalls[x + y * (Size + 1)];
+                default: return null;
+            }
+        }
+
+        public static WallMaze LoadFromMaze(Maze maze)
+        {
+            var wallMaze = new WallMaze();
+            wallMaze.Size = maze.Size;
+            wallMaze.VerticalWalls = new List<Wall>(wallMaze.Size * (wallMaze.Size + 1));
+            wallMaze.HorizontalWalls = new List<Wall>(wallMaze.Size * (wallMaze.Size + 1));
+
+            for (var y = 0; y < maze.Size; ++y)
+            {
+                for (var x = 0; x < maze.Size; ++x)
+                {
+                    if (maze.At(x, y).East) wallMaze.At(x, y, DirectionType.East).Exist = true;
+                    if (maze.At(x, y).West) wallMaze.At(x, y, DirectionType.West).Exist = true;
+                    if (maze.At(x, y).North) wallMaze.At(x, y, DirectionType.North).Exist = true;
+                    if (maze.At(x, y).South) wallMaze.At(x, y, DirectionType.South).Exist = true;
+                }
+            }
+            return wallMaze;
+        }
+    }
+}
